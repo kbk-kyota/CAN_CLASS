bbdf3f7c93c89135c19f64f5d8677fc37d24c525
ryugirou <ryunikaidou@gmail.com>
2019-06-30 23:03:13 +0900

    first commit

---
+++ Src/wrapper.cpp
@@ -0,0 +1,31 @@
+/*
+ * wrapper.cpp
+ *
+ *  Created on: May 29, 2019
+ *      Author: ryu
+ */
+#include "stm32f1xx_hal.h"
+#include "wrapper.hpp"
+#include "CanClass.hpp"
+double X = 6.28;
+double Y = 0.0;
+double Z = 0.0;
+CanClass can;
+void main_cpp(void) {
+	can.init();
+    while(1){
+    	can.send(X,0x205);
+    	can.send(Y,0x206);
+		HAL_Delay(50);
+    }
+
+}
+
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
+{
+	can.receive(Z, 0x4e2);
+	can.receive(Y, 0x4e1);
+	can.endit();//割り込み終了
+}
+
+
