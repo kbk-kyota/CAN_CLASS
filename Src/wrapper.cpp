026a26219beb4e74711f060873b4820f13b82114
khk <kyotakuboki@gmail.com>
2019-08-25 06:57:36 +0900

    公安用の電磁弁駆動基板のfirmware

--- Src/wrapper.cpp
+++ Src/wrapper.cpp
@@ -8,15 +8,21 @@
 #include "wrapper.hpp"
 #include "CanClass.hpp"
 uint8_t cmd;
+bool shutdown_cmd = false;
 
 CanClass can;
 void main_cpp(void) {
+	//この電磁弁駆動基板はマイコンの出力が反転されるため、最初に電磁弁への出力を初期化する
+	GPIOA ->BSRR = GPIO_BSRR_BS8;
+
+	HAL_Delay(500);
+
 	GPIOB->BSRR = GPIO_BSRR_BS0;//green
 	GPIOB->BSRR = GPIO_BSRR_BS1;//yellow
 	GPIOB->BSRR = GPIO_BSRR_BS2;//red
 	GPIOA->BSRR = GPIO_BSRR_BS11;//led_can
 
-	HAL_Delay(500);
+	HAL_Delay(100);
 
 	GPIOB->BSRR = GPIO_BSRR_BR0;
 	GPIOB->BSRR = GPIO_BSRR_BR1;
@@ -26,10 +32,17 @@ void main_cpp(void) {
 	can.init();
 
     while(1){
-    	//can.send(X,0x205);
-    	//can.send(Y,0x206);
-    	//can.led_process();
-		//HAL_Delay(50);
+
+/*    	if((GPIOA->IDR & GPIO_IDR_IDR14) == 0)
+  	  {
+    		GPIOB->BSRR = GPIO_BSRR_BR1;
+    		GPIOB->BSRR = GPIO_BSRR_BS2;
+    		GPIOA ->BSRR = GPIO_BSRR_BS8;
+  	  }
+  	  else
+  	  {
+  	  }*/
+
     }
 
 }
@@ -37,91 +50,31 @@ void main_cpp(void) {
 void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
 {
 	can.receive(cmd, 0x5e2);
-
-	int i = 0;
-	while(i < 8)
+	if(shutdown_cmd == true)
 	{
-		if((cmd& 0x01) == 0x00)
-		{
-			switch(i)
-			{
-			case 0:
-				GPIOA ->BSRR = GPIO_BSRR_BS8;
-				break;
-			case 1:
-				GPIOA ->BSRR = GPIO_BSRR_BS9;
-				break;
-			case 2:
-				GPIOA ->BSRR = GPIO_BSRR_BS10;
-				break;
-			case 3:
-				GPIOA ->BSRR = GPIO_BSRR_BS12;
-				break;
-			case 4:
-				GPIOA ->BSRR = GPIO_BSRR_BS2;
-				break;
-			case 5:
-				GPIOA ->BSRR = GPIO_BSRR_BS3;
-				break;
-			case 6:
-				GPIOA ->BSRR = GPIO_BSRR_BS5;
-				break;
-			case 7:
-				GPIOB ->BSRR = GPIO_BSRR_BS5;
-				break;
-			}
-		}
-		else
-		{
-			switch(i)
-			{
-			case 0:
-				GPIOA ->BSRR = GPIO_BSRR_BR8;
-				break;
-			case 1:
-				GPIOA ->BSRR = GPIO_BSRR_BR9;
-				break;
-			case 2:
-				GPIOA ->BSRR = GPIO_BSRR_BR10;
-				break;
-			case 3:
-				GPIOA ->BSRR = GPIO_BSRR_BR12;
-				break;
-			case 4:
-				GPIOA ->BSRR = GPIO_BSRR_BR2;
-				break;
-			case 5:
-				GPIOA ->BSRR = GPIO_BSRR_BR3;
-				break;
-			case 6:
-				GPIOA ->BSRR = GPIO_BSRR_BR5;
-				break;
-			case 7:
-				GPIOB ->BSRR = GPIO_BSRR_BR5;
-				break;
-						}
-
-		}
-		cmd >>= 1;
-		i++;
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
-		__NOP();
+	switch(cmd)
+	{
+	case 0x00://shutdown
+		GPIOB->BSRR = GPIO_BSRR_BR1;
+		GPIOB->BSRR = GPIO_BSRR_BS2;
+		GPIOA ->BSRR = GPIO_BSRR_BS8;
+		shutdown_cmd = false;
+		break;
+	case 0x01:
+		GPIOA ->BSRR = GPIO_BSRR_BR8;
+		break;
+	default:
+		GPIOA ->BSRR = GPIO_BSRR_BS8;
+		break;
+	}
 	}
 
-	__NOP();
-	__NOP();
-	__NOP();
-	__NOP();
-	__NOP();
-	GPIOB->BSRR = GPIO_BSRR_BS1;
+	if(cmd == 0x10)//enable
+	{
+		GPIOB->BSRR = GPIO_BSRR_BS1;
+		GPIOB->BSRR = GPIO_BSRR_BR2;
+		shutdown_cmd = true;
+	}
 	can.endit();//割り込み終了
 }
 
