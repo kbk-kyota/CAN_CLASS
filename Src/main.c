9f444f32ebff9340b0f11da429289568b83e7355
khk <kyotakuboki@gmail.com>
2019-08-24 10:24:36 +0900

    srcの変更

--- Src/main.c
+++ Src/main.c
@@ -217,7 +217,7 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin : PC14 */
   GPIO_InitStruct.Pin = GPIO_PIN_14;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PA2 PA3 PA5 PA8 
--- Src/wrapper.cpp
+++ Src/wrapper.cpp
@@ -11,19 +11,19 @@ uint8_t cmd;
 
 CanClass can;
 void main_cpp(void) {
-	can.init();
-
 	GPIOB->BSRR = GPIO_BSRR_BS0;//green
 	GPIOB->BSRR = GPIO_BSRR_BS1;//yellow
 	GPIOB->BSRR = GPIO_BSRR_BS2;//red
 	GPIOA->BSRR = GPIO_BSRR_BS11;//led_can
 
-	HAL_Delay(2000);
+	HAL_Delay(500);
 
 	GPIOB->BSRR = GPIO_BSRR_BR0;
 	GPIOB->BSRR = GPIO_BSRR_BR1;
 	GPIOB->BSRR = GPIO_BSRR_BR2;
 	GPIOA->BSRR = GPIO_BSRR_BR11;
+
+	can.init();
 
     while(1){
     	//can.send(X,0x205);
@@ -36,26 +36,26 @@ void main_cpp(void) {
 
 void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
 {
-	can.receive(cmd, 0x4e2);
+	can.receive(cmd, 0x5e2);
 
 	int i = 0;
 	while(i < 8)
 	{
-		if((cmd& 0x01) != 0x00)
+		if((cmd& 0x01) == 0x00)
 		{
 			switch(i)
 			{
 			case 0:
-				GPIOB ->BSRR = GPIO_BSRR_BS8;
+				GPIOA ->BSRR = GPIO_BSRR_BS8;
 				break;
 			case 1:
-				GPIOB ->BSRR = GPIO_BSRR_BS9;
+				GPIOA ->BSRR = GPIO_BSRR_BS9;
 				break;
 			case 2:
-				GPIOB ->BSRR = GPIO_BSRR_BS10;
+				GPIOA ->BSRR = GPIO_BSRR_BS10;
 				break;
 			case 3:
-				GPIOB ->BSRR = GPIO_BSRR_BS12;
+				GPIOA ->BSRR = GPIO_BSRR_BS12;
 				break;
 			case 4:
 				GPIOA ->BSRR = GPIO_BSRR_BS2;
@@ -76,16 +76,16 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
 			switch(i)
 			{
 			case 0:
-				GPIOB ->BSRR = GPIO_BSRR_BR8;
+				GPIOA ->BSRR = GPIO_BSRR_BR8;
 				break;
 			case 1:
-				GPIOB ->BSRR = GPIO_BSRR_BR9;
+				GPIOA ->BSRR = GPIO_BSRR_BR9;
 				break;
 			case 2:
-				GPIOB ->BSRR = GPIO_BSRR_BR10;
+				GPIOA ->BSRR = GPIO_BSRR_BR10;
 				break;
 			case 3:
-				GPIOB ->BSRR = GPIO_BSRR_BR12;
+				GPIOA ->BSRR = GPIO_BSRR_BR12;
 				break;
 			case 4:
 				GPIOA ->BSRR = GPIO_BSRR_BR2;
@@ -121,7 +121,7 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
 	__NOP();
 	__NOP();
 	__NOP();
-
+	GPIOB->BSRR = GPIO_BSRR_BS1;
 	can.endit();//割り込み終了
 }
 
